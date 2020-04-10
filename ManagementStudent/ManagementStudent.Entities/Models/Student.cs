c6b2b39d1d09af671f6910eb41bafc2fd9ac0828
Thinh Nguyen <thanhthinhcntt@gmail.com>
2020-04-10 20:51:25 +0700

    add student

---
+++ ManagementStudent/ManagementStudent.Entities/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace ManagementStudent.Entities.Models
+{
+	public class Student : BaseEntity
+	{
+		public string Name { get; set; }
+
+		public string Phone { get; set; }
+
+		public string Address { get; set; }
+
+		public string Email { get; set; }
+	}
+}
