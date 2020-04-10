6b4f2943698e763bb7a6e6893b9912fb03e41387
Thinh Nguyen <thanhthinhcntt@gmail.com>
2020-04-10 22:10:22 +0700

    add controller

---
+++ ManagementStudent/ManagementStudent.Api/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementStudent.Api.Controllers
+{
+    [Route("api/student")]
+    public class StudentController : BaseController
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetById(int Id)
+        {
+            return Ok(Id);
+        }
+    }
+}
