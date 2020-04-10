2e6ec2d59e0bfaf00208d337ae24611cc7d1822e
Thinh Nguyen <thanhthinhcntt@gmail.com>
2020-04-10 23:39:09 +0700

    add exception showmessage

---
+++ ManagementStudent/ManagementStudent.ViewModels/Exceptions/MStudentException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ManagementStudent.ViewModels.Exceptions
+{
+    public class MStudentException: Exception
+    {
+        public MStudentException()
+        {
+        }
+
+        public MStudentException(string message)
+            : base(message)
+        {
+        }
+
+        public MStudentException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
