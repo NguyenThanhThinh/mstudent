19433b97d5ffb2526f94e4ed4c56d5b9f6f57293
Thinh Nguyen <thanhthinhcntt@gmail.com>
2020-04-10 22:36:45 +0700

    add config

---
+++ ManagementStudent/ManagementStudent.Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ManagementStudent.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ManagementStudent.Data.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+            builder.Property(x => x.FirstName).IsRequired().HasMaxLength(200);
+            builder.Property(x => x.LastName).IsRequired().HasMaxLength(200);
+           
+        }
+    }
+}
