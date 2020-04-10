a07f600d4b5f39b8b6f906117ef245b9df3b38d9
Thinh Nguyen <thanhthinhcntt@gmail.com>
2020-04-10 21:19:02 +0700

    add config layer data

---
+++ ManagementStudent/ManagementStudent.Data/ManagementStudentDbFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ManagementStudent.Data
+{
+	public class ManagementStudentDbFactory: IDesignTimeDbContextFactory<ManagementStudentDbContext>
+    {
+        public ManagementStudentDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("ManagementStudentDb");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ManagementStudentDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ManagementStudentDbContext(optionsBuilder.Options);
+        }
+    }
+}
